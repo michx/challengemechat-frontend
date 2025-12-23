94fabd271fe9758eda2d3e00fc625674bd25366d
michx <io@io.com>
2025-12-23 22:42:07 +0100

    f

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -49,6 +49,7 @@ export default function Login() {
     try {
       const response = await fetch("/api/auth/send-otp", {
         method: "POST",
+        mode: 'same-origin',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phone }),
       });
